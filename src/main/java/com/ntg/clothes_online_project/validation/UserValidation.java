b93ad4119b9c1043fc9f9789940a006f30931458
NouraAshraff <62406338+NouraAshraff@users.noreply.github.com>
2022-08-18 09:18:21 +0200

    final final

--- src/main/java/com/ntg/clothes_online_project/validation/UserValidation.java
+++ src/main/java/com/ntg/clothes_online_project/validation/UserValidation.java
@@ -15,16 +15,16 @@ public class UserValidation {
 
 
     public ResponseEntity<MessageResponse> validateUser(User user) {
-        if (user.getEmail()==null) {
+        if (user.getEmail()==null || user.getEmail().length()==0) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error: Email can not be null!"));
         }
-        if (user.getName()==null) {
+        if (user.getName()==null || user.getName().length()==0) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error: Name can not be null!"));
         }
-        if (user.getUserName()==null) {
+        if (user.getUserName()==null || user.getUserName().length()==0) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error: User Name can not be null!"));
         }
-        if (user.getPassword()==null) {
+        if (user.getPassword()==null || user.getPassword().length()==0) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error: password can not be null!"));
         }
         if (userRepository.existsByEmail(user.getEmail())) {
